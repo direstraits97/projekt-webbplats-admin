1426a5ba1e09df1ded696802bdeac21e959eccfc
joba <josefine.backlund@hotmail.com>
2026-05-25 11:21:14 +0200

    Uppstart projekt.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Mainheader } from './partials/mainheader/mainheader';
+import { Mainfooter } from './partials/mainfooter/mainfooter';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Mainheader, Mainfooter],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('projekt-webbplats');
